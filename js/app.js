786faa2b8cda6362c987b45cd3cb22d73bc2c38e
CupapiOT <166366567+CupapiOT@users.noreply.github.com>
2024-09-26 10:37:52 +0800

    Add interactivity and animate hamburger menu
    Notes:
    - JS is used to close the hamburger menu after one of its links are pressed.
    - Pressing the home navigation link doesn't count as pressing one of the navigation links.
    - For some reason, the glass effect doesn't work on the dropdown.
    - Hamburger menu does not yet have accessibility features.
    
    Additional changes:
    - [Refactor] Move code in `app.js` in the order of when the code appears in the HTML file (top-to-bottom) and the generality of the code.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,5 @@
-function copyTextToClipboard(text) {
-  navigator.clipboard.writeText(text)
-    .then(() => {
-      console.log("Text successfully copied to clipboard.");
-    })
-    .catch((error) => {
-      console.error("Failed to copy text: ", error);
-    });
-}
-
 function applyActionToElements(action, ...elements) {
-  elements.forEach(element => {
+  elements.forEach((element) => {
     if (element instanceof Element) {
       action(element);
     } else {
@@ -18,27 +8,57 @@ function applyActionToElements(action, ...elements) {
   });
 }
 
-function temporarilyOverrideImgAndStyle(milliseconds= 3000, ...elements) {
-  applyActionToElements((element) => element.classList.add('override-image'), ...elements);
+// Navigation Bar "Close after pressing button" logic.
+
+function uncheckCheckbox(checkbox) {
+  checkbox.checked = false;
+}
+
+const hamburgerMenu = document.getElementById("hamburger-menu__toggle");
+const hamburgerMenuLinks = document.getElementsByClassName("dropdown__link");
+for (let i = 0; i < hamburgerMenuLinks.length; i++) {
+  hamburgerMenuLinks[i].addEventListener("click", () => {
+    uncheckCheckbox(hamburgerMenu);
+  });
+}
+
+// Copy Email Button
+
+function copyTextToClipboard(text) {
+  navigator.clipboard
+    .writeText(text)
+    .then(() => {
+      console.log("Text successfully copied to clipboard.");
+    })
+    .catch((error) => {
+      console.error("Failed to copy text: ", error);
+    });
+}
+
+function temporarilyOverrideImgAndStyle(milliseconds = 3000, ...elements) {
+  applyActionToElements(
+    (element) => element.classList.add("override-image"),
+    ...elements
+  );
   setTimeout(
-    () => applyActionToElements((element) => element.classList.remove('override-image'),
-      ...elements
-    ),
+    () =>
+      applyActionToElements(
+        (element) => element.classList.remove("override-image"),
+        ...elements
+      ),
     milliseconds
   );
 }
 
-const copyEmailButton = document.getElementById('copy-email-button');
-const copyEmailButtonImg = copyEmailButton.querySelector('img');
-const copyEmailButtonPopUp = copyEmailButton.querySelector('span');
-copyEmailButton.addEventListener('click', () => {
+const copyEmailButton = document.getElementById("copy-email-button");
+const copyEmailButtonImg = copyEmailButton.querySelector("img");
+const copyEmailButtonPopUp = copyEmailButton.querySelector("span");
+copyEmailButton.addEventListener("click", () => {
   copyTextToClipboard("hellomarvelorleans@gmail.com");
   temporarilyOverrideImgAndStyle(
-      1500,
+    1500,
     copyEmailButton,
     copyEmailButtonImg,
     copyEmailButtonPopUp
   );
 });
-
-
